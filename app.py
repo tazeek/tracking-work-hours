742042a07a3ad2196b936b9e51f862f26f4fc97c
tazeek <tazeek.rakib@gmail.com>
2020-07-13 23:52:51 +0800

    Removed unused variables

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 import dash
+
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
+# Define your variables
+
+color='lightblue'
+
+my_dashboard_title='Working Hours Dashboard'
+my_bar_heading='Daily hours calculation'
 
 ########### Set up the chart
 bitterness = go.Bar(
@@ -22,6 +18,7 @@ bitterness = go.Bar(
     name=label1,
     marker={'color':color1}
 )
+
 alcohol = go.Bar(
     x=beers,
     y=abv_values,
@@ -30,23 +27,24 @@ alcohol = go.Bar(
 )
 
 beer_data = [bitterness, alcohol]
+
 beer_layout = go.Layout(
     barmode='group',
-    title = mytitle
+    title = my_dashboard_title
 )
 
 beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
 
-########### Initiate the app
+# Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title=tabtitle
+app.title='Hours analysis'
 
-########### Set up the layout
+# Set up the layout
 app.layout = html.Div(children=[
-    html.H1(myheading),
+    html.H1(my_bar_heading),
     dcc.Graph(
         id='flyingdog',
         figure=beer_fig
