e7e0b96c12cbd88a5a3e4975f3e2f38924effb18
tazeek <tazeek.rakib@gmail.com>
2020-07-14 00:04:24 +0800

    Remove all boiler plate variables

--- app.py
+++ app.py
@@ -11,29 +11,7 @@ color='lightblue'
 my_dashboard_title='Working Hours Dashboard'
 my_bar_heading='Daily hours calculation'
 
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-
-beer_layout = go.Layout(
-    barmode='group',
-    title = my_dashboard_title
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+# Set up the charts
 
 
 # Initiate the app
@@ -44,11 +22,7 @@ app.title='Hours analysis'
 
 # Set up the layout
 app.layout = html.Div(children=[
-    html.H1(my_bar_heading),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
-    )
+    html.H1(my_bar_heading)
     ]
 )
 
