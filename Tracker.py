f9bc133153ab7c7dc0b4e79ad90850253310e547
tazeek <tazeek.rakib@gmail.com>
2020-11-16 07:54:26 +0800

    Functions created (Part I)

--- Tracker.py
+++ Tracker.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta, time
+
 class Tracker:
 
 	def __init__(self):
@@ -10,3 +12,24 @@ class Tracker:
 		self._max_minutes_daily = self._daily_hours_cover * self._total_minutes_hour
 		self._max_minutes_weekly = self._num_working_days * self._max_minutes_daily
 		self._overtime_hours = self._total_minutes_hour * (self._daily_hours_cover + 1)
+
+	def _get_current_time(self):
+		return str(datetime.now().hour) + ':' + str(datetime.now().minute)
+
+	def _get_day_number(self):
+		return datetime.today().weekday()
+
+	def _get_hours_minutes(self,total_minutes):
+		hours, minutes = divmod(total_minutes, self._total_minutes_hour)
+
+		return hours, minutes
+
+	def _convert_duration_to_minutes(self,start_time, end_time):
+
+		FMT = '%H:%M'
+
+		diff = datetime.strptime(end_time, FMT) - datetime.strptime(start_time, FMT)
+
+		return diff.seconds/60
+
+	def
