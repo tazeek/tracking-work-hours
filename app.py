96b4fbe2d6f6d6db1514c2b24e2c3c2085be7427
tazeek <tazeek.rakib@gmail.com>
2020-11-25 07:12:09 +0800

    Loading an example dataframe

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ colors = {
 
 app = dash.Dash()
 
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
+
 app.layout = html.Div(
     style = {'backgroundColor': colors['background']},
     children = [
