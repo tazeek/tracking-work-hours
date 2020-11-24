756a5ee877d4562045cc04f67950fac2cab9761c
tazeek <tazeek.rakib@gmail.com>
2020-11-25 07:23:11 +0800

    Removed unused code

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
 app = dash.Dash()
 
 df = pd.read_csv(
@@ -17,35 +12,6 @@ df = pd.read_csv(
     '5d1ea79569ed194d432e56108a04d188/raw/' +
     'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
     'gdp-life-exp-2007.csv')
-
-app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=df[df['continent'] == i]['gdp per capita'],
-                    y=df[df['continent'] == i]['life expectancy'],
-                    text=df[df['continent'] == i]['country'],
-                    mode='markers',
-                    opacity=0.8,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in df.continent.unique()
-            ],
-            'layout': go.Layout(
-                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-                yaxis={'title': 'Life Expectancy'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
-            )
-        }
-    )
-])
 
 app.layout = html.Div([
     dcc.Graph(
