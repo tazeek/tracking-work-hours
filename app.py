6f569f9e8b847fe8b601f2a44fd14cabb978159c
tazeek <tazeek.rakib@gmail.com>
2020-11-26 07:25:55 +0800

    Weekly hours loaded successfully into dash app

--- app.py
+++ app.py
@@ -11,40 +11,21 @@ app = dash.Dash()
 
 tracker_obj = Tracker()
 tracker_obj.update_time_calculations()
+days_df = pd.DataFrame(tracker_obj.get_days_stats())
 
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/' +
-    '5d1ea79569ed194d432e56108a04d188/raw/' +
-    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
-    'gdp-life-exp-2007.csv')
+weekly_hours_fig = go.Figure(
+    go.Bar(
+        x=days_df['minutes_covered'],
+        y=days_df['day'],
+        orientation='h',
+        name="",
+        customdata=days_df['coverage'],
+        hovertemplate="Total: %{x}<br>Coverage: %{customdata}"
+    )
+)
 
 app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=df[df['continent'] == i]['gdp per capita'],
-                    y=df[df['continent'] == i]['life expectancy'],
-                    text=df[df['continent'] == i]['country'],
-                    mode='markers',
-                    opacity=0.8,
-                    marker={
-                        'size': 15,
-                        'line': {'width':0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in df.continent.unique()
-            ],
-            'layout': go.Layout(
-                xaxis={'type':'log', 'title':'GDP Per Capita'},
-                yaxis={'title': 'Life expectancy'},
-                margin={'l':40, 'b':40, 't':10, 'r':10},
-                legend={'x':0, 'y':1},
-                hovermode='closest'
-            )
-        }
-    )
+    dcc.Graph(id='overall-week-hours',figure=weekly_hours_fig)
 ])
 
 if __name__ == '__main__':
