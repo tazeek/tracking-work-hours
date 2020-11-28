bd8a2180bb3b2d072b75f56f3529b1d144570924
tazeek <tazeek.rakib@gmail.com>
2020-11-28 13:37:37 +0800

    Time analysis dict added

--- WeeklyTab.py
+++ WeeklyTab.py
@@ -101,4 +101,42 @@ class WeeklyTab:
 
 		))
 
+		overall_hours_pie_fig.update_layout(title_text='Overall weekly hours calculation: Remaining vs Covered')
+
 		return overall_hours_pie_fig
+
+	def generate_noon_comparisons(self):
+
+		tracker_obj = self._tracker_obj
+
+		time_analysis_dict = {
+		    'before_noon': tracker_obj.get_before_noon_minutes(),
+		    'after_noon': tracker_obj.get_after_noon_minutes()
+		}
+
+		time_analysis_df = pd.DataFrame(time_analysis_dict.items(), columns=['category', 'minutes'])
+
+		time_analysis_fig = go.Figure(go.Bar(
+		    name = "",
+		    x=time_analysis_df['category'],
+		    y=time_analysis_df['minutes'],
+		    hovertemplate = "Total(minutes): %{y}"
+		    
+		))
+
+		# Hours should be 50-50 coverage
+		time_analysis_fig.add_shape(
+		    dict(
+		        type="line",
+		        x0=-0.5,
+		        x1=1.5,
+		        y0=1200, # Half of 40 hours is 20 hours (1200 minutes)
+		        y1=1200,
+		        line=dict(
+		            color="#FECB52",
+		            width=2,
+		            dash="dot"
+		        ),
+		))
+
+		return time_analysis_fig 
