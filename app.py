26c0c3adb5a9b4a9b1dd6735e976d8b1caf29a5b
tazeek <tazeek.rakib@gmail.com>
2020-11-28 14:25:11 +0800

    Clean up app.py

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-app = dash.Dash()
+def initialize_app(app):
 
-weekly_stats_obj = WeeklyTab()
+    weekly_stats_obj = WeeklyTab()
 
-app.layout = html.Div(children=[
-    dcc.Graph(id='overall-week-hours',figure=weekly_stats_obj.generate_weekly_hours()),
-    dcc.Graph(id='total-hours-pie',figure=weekly_stats_obj.generate_overall_hours()),
-    dcc.Graph(id='time-analysis',figure=weekly_stats_obj.generate_noon_comparisons())
-])
+    app.layout = html.Div(children=[
+        dcc.Graph(id='overall-week-hours',figure=weekly_stats_obj.generate_weekly_hours()),
+        dcc.Graph(id='total-hours-pie',figure=weekly_stats_obj.generate_overall_hours()),
+        dcc.Graph(id='time-analysis',figure=weekly_stats_obj.generate_noon_comparisons())
+    ])
 
 if __name__ == '__main__':
+
+    app = dash.Dash()
+    initialize_app(app)
+    
     app.run_server(debug=True)
