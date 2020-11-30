24ec07b865d7b3d8d77a4488513fb9cb862fb9eb
tazeek <tazeek.rakib@gmail.com>
2020-11-30 10:03:43 +0800

    Loaded callback successfully

--- app.py
+++ app.py
@@ -10,28 +10,32 @@ import dash_html_components as html
 
 def initialize_app(app):
 
-	tracker_obj = Tracker()
-	tracker_obj.update_time_calculations()
+    tracker_obj = Tracker()
+    tracker_obj.update_time_calculations()
 
-	weekly_stats_obj = WeeklyTab(tracker_obj)
-	today_stats_obj = TodayTab(tracker_obj)
-	
-	app.layout = html.Div([
-    	dcc.Tabs(id='tabs-list', value='week-tab', children= [
-    		dcc.Tab(label='Weekly Stats', value='week-tab', children=[
-    			dcc.Graph(id='overall-week-hours',figure=weekly_stats_obj.generate_weekly_hours()),
-    			dcc.Graph(id='total-hours-pie',figure=weekly_stats_obj.generate_overall_hours()),
-    			dcc.Graph(id='time-analysis',figure=weekly_stats_obj.generate_noon_comparisons())
-    		]),
-    		dcc.Tab(label='Today Stats', value='today-tab', children=[
-    			dcc.Graph(id='today-remaining-hours',figure=today_stats_obj.generate_today_remaining_figure())
-    		])
-    	])
+    weekly_stats_obj = WeeklyTab(tracker_obj)
+    today_stats_obj = TodayTab(tracker_obj)
+    
+    app.layout = html.Div([
+        dcc.Tabs(id='tabs', value='week-tab', children=[
+            dcc.Tab(label='Weekly Stats', value='overall'),
+            dcc.Tab(label='Today Stats', value='today')
+        ]),
+        html.Div(id='data-graphs')
     ])
 
-if __name__ == '__main__':
+    return None
 
-    app = dash.Dash()
-    initialize_app(app)
+app = dash.Dash()
+initialize_app(app)
+
+@app.callback(
+    Output('data-graphs','children'),
+    [Input('tabs','value')]
+)
+def create_graphs_tabs(stat_name):
+    print(stat_name)
+
+if __name__ == '__main__':
     
     app.run_server(debug=True)
