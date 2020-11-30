210ecbfbb7d8db7f90def411e3c94aefb7267ef3
tazeek <tazeek.rakib@gmail.com>
2020-11-30 10:11:19 +0800

    Rolled out figures successfully

--- app.py
+++ app.py
@@ -9,15 +9,9 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 def initialize_app(app):
-
-    tracker_obj = Tracker()
-    tracker_obj.update_time_calculations()
-
-    weekly_stats_obj = WeeklyTab(tracker_obj)
-    today_stats_obj = TodayTab(tracker_obj)
     
     app.layout = html.Div([
-        dcc.Tabs(id='tabs', value='week-tab', children=[
+        dcc.Tabs(id='tabs', value='overall', children=[
             dcc.Tab(label='Weekly Stats', value='overall'),
             dcc.Tab(label='Today Stats', value='today')
         ]),
@@ -25,6 +19,12 @@ def initialize_app(app):
     ])
 
     return None
+
+tracker_obj = Tracker()
+tracker_obj.update_time_calculations()
+
+weekly_stats_obj = WeeklyTab(tracker_obj)
+today_stats_obj = TodayTab(tracker_obj)
 
 app = dash.Dash()
 initialize_app(app)
@@ -34,7 +34,17 @@ initialize_app(app)
     [Input('tabs','value')]
 )
 def create_graphs_tabs(stat_name):
-    print(stat_name)
+    
+    if stat_name == 'overall':
+
+        return [
+            dcc.Graph(id='overall-week-hours',figure=weekly_stats_obj.generate_weekly_hours()),
+            dcc.Graph(id='total-hours-pie',figure=weekly_stats_obj.generate_overall_hours()),
+            dcc.Graph(id='time-analysis',figure=weekly_stats_obj.generate_noon_comparisons())
+        ]
+
+    return [dcc.Graph(id='today-remaining-hours',figure=today_stats_obj.generate_today_remaining_figure())]
+
 
 if __name__ == '__main__':
     
