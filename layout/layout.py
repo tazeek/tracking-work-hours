3f0b2ef0f92ab0df0a25073d749051ee3bb88c02
tazeek <tazeek.rakib@gmail.com>
2021-01-09 12:40:33 +0800

    Improved display - Part I

--- layout/layout.py
+++ layout/layout.py
@@ -1,5 +1,21 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+SIDEBAR_STYLE = {
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "20rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
 
 def generate_layout(weekly_stats_obj):
 
@@ -15,12 +31,10 @@ def generate_layout(weekly_stats_obj):
     
 	return html.Div([
 
-		html.Div(children=[
-			html.H1(children='Time Tracking Hour Analyzer'),
+		html.H1(children='Time Tracking Hour Analyzer'),
 
-			html.Hr(),
-
-			html.H4(
+		html.Div([
+			html.Div(
 				id='live-update-text',
 				children=last_updated_str
 			),
@@ -28,56 +42,70 @@ def generate_layout(weekly_stats_obj):
 			html.Button('Update live', id='update-current', n_clicks=0)
 		]),
 
-		html.Hr(),
-
-		html.H4(
-			id='today-coverage',
-			children=today_coverage_str
-		),
-
-		html.Div(children=[
-
-			html.Div(
-				dcc.Input(
-					id='input-coverage-hours',
-					type='text',
-					placeholder='HH:MM (Ex. 05:45, 11:45)'
-				), 
-				style={'display': 'inline-block'}
-			),
-
-			html.Div(
-				dcc.ConfirmDialogProvider(
-					children=html.Button(
-						children=button_status.capitalize(),
-						value=button_status,
-						id='update-coverage'
-					),
-					id='update-coverage-dialog',
-					message='Do you want to continue to update today\'s coverage?'
-				),
-				style={'display': 'inline-block'}
-			)
-		]),
-
 		html.Div(children=[
 			html.P(id='error-output-update', style={'color':'red'})
 		]),
 
-		html.Hr(),
+		html.Div(
+			[
+				html.Div(
+					id='finishing-time',
+					children=finishing_time_str
+				),
 
-		html.H4(
-			id='finishing-time',
-			children=finishing_time_str
-		),
+				html.Div(
+					id='today-coverage',
+					children=today_coverage_str
+				),
 
+				html.Div([
 
-		dcc.ConfirmDialogProvider(
-			children=html.Button(
-				'Reset Hours',
-			),
-			id='reset-hours',
-			message='Do you want to reset your overall hours?'
+					html.Div(
+						dcc.Input(
+							id='input-coverage-hours',
+							type='text',
+							placeholder='HH:MM (Ex. 05:45, 11:45)'
+						), 
+						style={'display': 'inline-block'}
+					),
+
+					html.Div(
+						dcc.ConfirmDialogProvider(
+							children=html.Button(
+								children=button_status.capitalize(),
+								value=button_status,
+								id='update-coverage'
+							),
+							id='update-coverage-dialog',
+							message='Do you want to continue to update today\'s coverage?'
+						),
+						style={'display': 'inline-block'}
+					)
+				]),
+
+				html.Div(className='graph-displayer', children = [
+					dcc.Graph(
+					id='total-hours-pie',
+					figure=total_hours_fig,
+					config={'displayModeBar': False}
+					)
+				]),
+
+				html.Br(),
+
+				html.Div(id='coverage-table-div', children=[weekly_coverage_table]),
+
+				html.Br(),
+
+				dcc.ConfirmDialogProvider(
+					children=html.Button(
+						'Reset Hours',
+					),
+					id='reset-hours',
+					message='Do you want to reset your overall hours?'
+				)
+			],
+			style=SIDEBAR_STYLE
 		),
 
 		html.Div(children=[
@@ -88,19 +116,10 @@ def generate_layout(weekly_stats_obj):
 				figure=overall_hours_fig,
 				config={'displayModeBar': False, 'staticPlot': True}
 				)
-			]),
-
-			html.Div(className='graph-displayer', children = [
-				dcc.Graph(
-				id='total-hours-pie',
-				figure=total_hours_fig,
-				config={'displayModeBar': False}
-				)
 			])
-		]),
+		],
+		style=CONTENT_STYLE
 
-		html.Hr(),
-
-		html.H4(children='Weekly coverage'),
-		html.Div(id='coverage-table-div', children=[weekly_coverage_table])
+		)
+		
 	])
