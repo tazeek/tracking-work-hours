99755a64b15d82c729fd1e403864b6d4a95902a0
tazeek <tazeek.rakib@gmail.com>
2021-01-09 10:42:11 +0800

    Minty theme added

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from controller.controller import register_callbacks
 from layout.layout import generate_layout
 
 import dash
+import dash_bootstrap_components as dbc
 
 weekly_stats_obj = WeeklyTab()
 
@@ -13,7 +14,8 @@ def serve_layout():
 
 app = dash.Dash(
 	__name__,
-	title='Work Hours Analytics'
+	title='Work Hours Analytics',
+	external_stylesheets=[dbc.themes.MINTY]
 )
 
 app.layout = serve_layout
